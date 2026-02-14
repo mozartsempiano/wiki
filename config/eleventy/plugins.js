0b4efcbf2a95544883aece9e8de9c9872a2bc238
mozartsempiano <mozartraulmt@gmail.com>
2026-02-14 20:53:29 -0300

    Change fonts and fix Embed Everything plugin

--- config/eleventy/plugins.js
+++ config/eleventy/plugins.js
@@ -6,44 +6,41 @@ const EleventyPluginRobotsTxt = require("eleventy-plugin-robotstxt");
 
 /** @type {import("eleventy-plugin-robotstxt/typedefs.js").EleventyPluginRobotsTxtOptions} */
 const eleventyPluginRobotsTxtOptions = {
-	// sitemapURL: "https://mozartsempiano.com/sitemap.xml",
-	shouldBlockAIRobots: true,
-	rules: new Map([["*", [{ disallow: "/sonhos/" }, { disallow: "/junk/" }]]]),
+  // sitemapURL: "https://mozartsempiano.com/sitemap.xml",
+  shouldBlockAIRobots: true,
+  rules: new Map([["*", [{ disallow: "/sonhos/" }, { disallow: "/junk/" }]]]),
 };
 
 module.exports = function configurePlugins(eleventyConfig) {
-	// const tinyOptions = {
-	// 	purgeCSS: {
-	// 		fontFace: true,
-	// 		keyframes: true,
-	// 		variables: true,
-	// 	},
-	// };
-	// eleventyConfig.addPlugin(tinyCSS, tinyOptions);
+  // const tinyOptions = {
+  // 	purgeCSS: {
+  // 		fontFace: true,
+  // 		keyframes: true,
+  // 		variables: true,
+  // 	},
+  // };
+  // eleventyConfig.addPlugin(tinyCSS, tinyOptions);
 
-	// const tinyHTMLOptions = {
-	// 	html5: true,
-	// 	removeRedundantAttributes: true,
-	// 	collapseBooleanAttributes: true,
-	// 	collapseWhitespace: true,
-	// 	decodeEntities: true,
-	// 	html5: true,
-	// 	removeAttributeQuotes: true,
-	// 	removeComments: true,
-	// 	removeOptionalTags: true,
-	// 	sortAttributes: true,
-	// 	sortClassName: true,
-	// };
-	// eleventyConfig.addPlugin(tinyHTML, tinyHTMLOptions);
+  // const tinyHTMLOptions = {
+  // 	html5: true,
+  // 	removeRedundantAttributes: true,
+  // 	collapseBooleanAttributes: true,
+  // 	collapseWhitespace: true,
+  // 	decodeEntities: true,
+  // 	html5: true,
+  // 	removeAttributeQuotes: true,
+  // 	removeComments: true,
+  // 	removeOptionalTags: true,
+  // 	sortAttributes: true,
+  // 	sortClassName: true,
+  // };
+  // eleventyConfig.addPlugin(tinyHTML, tinyHTMLOptions);
 
-	// eleventyConfig.addPlugin(embedEverything);
+  eleventyConfig.addPlugin(embedEverything);
 
-	eleventyConfig.addPlugin(
-		EleventyPluginRobotsTxt,
-		eleventyPluginRobotsTxtOptions,
-	);
+  eleventyConfig.addPlugin(EleventyPluginRobotsTxt, eleventyPluginRobotsTxtOptions);
 
-	eleventyConfig.addPlugin(sitemap, {
-		sitemap: { hostname: "https://mozartsempiano.com" },
-	});
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: { hostname: "https://mozartsempiano.com" },
+  });
 };
--- content/regex.md
+++ content/regex.md
@@ -1,3 +1,9 @@
+---
+layout: default
+title: Regex
+created: 2026-02-14T19:45
+---
+
 Busca por tags `<code>` em markdown, que começam e terminam com uma única crase simples
 
 ```
