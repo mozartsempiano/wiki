654262e46f1e14a7571f60d3147db05a8d7f5b59
Mozart <mozartmt@protonmail.com>
2026-02-20 11:22:13 -0300

    Improve galeria-item pages layout and logic

--- .eleventy.js
+++ .eleventy.js
@@ -20,7 +20,6 @@ module.exports = function (eleventyConfig) {
 
 	const md = configureMarkdown();
 	const renderMarkdown = (value) => md.render(String(value ?? ""));
-	eleventyConfig.addFilter("renderMarkdown", renderMarkdown);
 	eleventyConfig.addNunjucksFilter("renderMarkdown", renderMarkdown);
 	eleventyConfig.setLibrary("md", md);
 
